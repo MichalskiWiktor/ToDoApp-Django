055131dfea7ca476809940f556d32875779e47b1
MichalskiWiktor <wiktor.kasa1@gmail.com>
2023-09-27 18:24:23 +0200

    update Item

--- list/urls.py
+++ list/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.showList, name="list"),
     path("newItem/", views.create_item, name="new_item"),
+    path("updateItem/<str:pk>/", views.update_item, name="update_item"),
 ]
--- list/views.py
+++ list/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse
 from .forms import NewItemForm 
 from .models import ListItem 
 
-
-# Create your views here.
 def showList(request):
     items = ListItem.objects.all()
     context = {"items":items}
@@ -15,19 +13,22 @@ def create_item(request):
         form = NewItemForm(request.POST)
         if form.is_valid():
             form.save()
-
-            redirect("list")
+            return redirect("list")
     else:
         form = NewItemForm()
-
     return render(request, "item_form.html", {"form": form})
 
-def update_item(request):
-    return HttpResponse("New Item")
+def update_item(request, pk):
+    obj = ListItem.objects.get(pk=pk)
+    # an empty NewItemForm means new Form
+    # with request.POST we fill the form with data that was submited
+    # and with instance we can fill the form with data that we choose
+    form = NewItemForm(request.POST or None, instance=obj) #
+    if form.is_valid():
+        form.save() # save method can create new object or update and existing one 
+        return redirect("list")
+    return render(request, "item_form.html", {"form": form})
 
 def delete_item(request):
-    return HttpResponse("New Item")
-
-def view_item(request):
     return HttpResponse("New Item")
  
